09a548ed36d461b346d8528d7519fbd331174771
zhangqixiang <zhang.qixiang@foxmail.com>
2026-03-15 22:12:37 +0800

    [fix] fix path

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
 
 export default defineConfig({
-  base: '/hanzi_writting/',
+  base: '/hanzi-writting/',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
